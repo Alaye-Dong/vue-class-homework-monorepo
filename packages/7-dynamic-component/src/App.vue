f3ee2f2395857b8b070b86743a838722dc980587
Alaye-Dong <AlayeVast@outlook.com>
2025-11-04 16:52:40 +0800

    feat(7-dynamic-component): 初步实现动态组件

---
+++ packages/7-dynamic-component/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import MyLeft from './components/MyLeft.vue';
+import MyRight from './components/MyRight.vue';
+
+import { ref } from 'vue'
+ 
+type TabKey = 'MyLeft' | 'MyRight'
+
+const currentTab = ref<TabKey>('MyLeft')
+
+const tabs: Record<TabKey, any> = {
+  MyLeft,
+  MyRight
+}
+
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div class="flex flex-wrap gap-2">
+      <button
+        v-for="(_, tab) in tabs"
+        :key="tab"
+        :class="['tab-button', { active: currentTab === tab }]"
+        class="btn"
+        @click="currentTab = tab"
+      >
+        展示{{ tab }}组件
+      </button>
+    </div>
+    <component :is="tabs[currentTab]"></component>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ packages/7-dynamic-component/src/components/MyLeft.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-3xl font-bold">MyLeft组件展示</div>
+</template>
+
+<style scoped>
+
+</style>
