70fb19d3b214696e631e7cd5f9e4209464070d68
Alaye-Dong <AlayeVast@outlook.com>
2025-11-13 12:38:04 +0800

    feat(experiment-5-data-statistics): 初始化项目结构和基础配置

---
+++ packages/experiment-5-data-statistics/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Count from './components/Count.vue'
+</script>
+
+<template>
+  <div>
+    Home-View
+  </div>
+  <Count />
+  <button>count+1</button>
+</template>
+
+<style scoped></style>
